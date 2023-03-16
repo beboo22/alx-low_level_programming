fb77bf35ba11f714f2e766b93ed31265e6f55b45
beboo22 <mt557829@gmail.com>
2023-03-16 15:03:40 -0700

    task9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        char msg[]("and that piece of art is useful"+" - Dora Korpar, 2015-10-19");
+        write(1, msg, 59);
+	return (0);
+}
