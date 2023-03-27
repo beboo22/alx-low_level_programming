03cfd865cf0103b5837bb7b365f623487df3060b
beboo22 <mt557829@gmail.com>
2023-03-27 11:09:08 -0700

    task4 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,22 +9,14 @@ void print_rev(char *s)
 {
 
 	char tmp;
-	int i, len, len1;
+	int i, l;
 
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
+	for (l=0;s[l] != '\0';++l)
+		;
+	for (i = 0; i < l / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = tmp;
 	}
 }
