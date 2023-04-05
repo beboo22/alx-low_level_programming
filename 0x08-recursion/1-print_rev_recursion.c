c842f199facc9ecb73ca4f2b285db00d190146be
beboo22 <mt557829@gmail.com>
2023-04-05 16:34:41 -0700

    test

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
+ *
+ */
+
+void _print_rev_recursion(char *x)
+{
+	if (*x)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
