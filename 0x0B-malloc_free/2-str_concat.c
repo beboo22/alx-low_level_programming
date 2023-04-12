6d80574328b9ca58f635134db6cd1607fde81b5d
beboo22 <mt557829@gmail.com>
2023-04-11 17:10:06 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
-	unsigned int i, j, z1, z2;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -32,12 +32,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (z1 = 0; z1 < i; z1++)
-		strout[z1] = s1[z1];
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	z2 = j;
-	for (j = 0; j <= z2; z1++; j++)
-		strout[z1] = s2[j];
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
 	return (strout);
 }
