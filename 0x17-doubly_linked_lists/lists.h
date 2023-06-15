39c87874c7492bfb6d50f23bb0c10364fac47a2f
beboo22 <mt557829@gmail.com>
2023-06-15 15:14:07 +0300

    task

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -17,3 +21,4 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+#endif 
