70e9860bc1f020c9aee22700f6498e27fab91fe1
beboo22 <mt557829@gmail.com>
2023-06-19 16:13:36 +0300

    task0

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -5,10 +5,10 @@
 #include <string.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always EXIT_SUCCESS.
- *     */
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
 	printf("%d\n", _strlen("My Dyn Lib"));
