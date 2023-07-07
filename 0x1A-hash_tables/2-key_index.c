7ca42b39095e0561e2c39ca6745d1e751ed9ad5e
beboo22 <mt557829@gmail.com>
2023-07-07 18:34:23 +0300

    task

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,13 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - Hashes a key and returns the corret
+ *
+ * @key: Pointer to an array of unsigned characters representing the key
+ * @size: The size of the hash table
+ *
+ * Return: The index in the hash table where the key-value pair will be stored
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
