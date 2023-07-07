4a3f8c5c1cf4557055bce4c0aa47bf3d09330c2b
beboo22 <mt557829@gmail.com>
2023-07-07 18:29:29 +0300

    task

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+        
+
+}
+hash_node_t *create_new_node (const char *key, const char *value)
+{
+
+}
